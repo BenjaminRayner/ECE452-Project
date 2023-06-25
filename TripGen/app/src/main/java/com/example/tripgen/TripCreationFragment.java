647c2422bdd5883cec049b97f9a138014063f18b
Justin Huynh <j64huynh@uwaterloo.ca>
2023-06-25 14:49:58 -0400

    Allow date range selection

--- TripGen/app/src/main/java/com/example/tripgen/TripCreationFragment.java
+++ TripGen/app/src/main/java/com/example/tripgen/TripCreationFragment.java
@@ -7,10 +7,12 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.tripgen.databinding.FragmentTripCreationBinding;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 public class TripCreationFragment extends Fragment {
 
@@ -29,22 +31,28 @@ public class TripCreationFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Pair<Long, Long> defaultSelection = Pair.create(MaterialDatePicker.thisMonthInUtcMilliseconds(), MaterialDatePicker.todayInUtcMilliseconds());
 
-        binding.tripSubmitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tripName = binding.editTripName.getText().toString().trim();
+        MaterialDatePicker<Pair<Long, Long>> materialDatePicker = MaterialDatePicker.
+                Builder.dateRangePicker().setSelection(defaultSelection).build();
 
-                if (binding.tripCalendarSelection.getDate() == 0 ) {
-                    Toast.makeText(requireContext(), R.string.invalid_date, Toast.LENGTH_SHORT).show();
-                } else if (tripName.isEmpty() ) {
-                    Toast.makeText(requireContext(), R.string.empty_trip_name, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(requireContext(), "binding.tripCalendarSelection.getDate()" + binding.tripCalendarSelection.getDate() + " tripName " + tripName, Toast.LENGTH_SHORT).show();
-                    NavHostFragment.findNavController(TripCreationFragment.this)
-                        .navigate(R.id.action_TripCreationFragment_to_DateFragment);
-                }
+
+        binding.tripSubmitButton.setOnClickListener(view1 -> {
+            String tripName = binding.editTripName.getText().toString().trim();
+
+            if (materialDatePicker.getHeaderText() == null || materialDatePicker.getHeaderText().equals("") ) {
+                Toast.makeText(requireContext(), R.string.invalid_date, Toast.LENGTH_SHORT).show();
+            } else if (tripName.isEmpty() ) {
+                Toast.makeText(requireContext(), R.string.empty_trip_name, Toast.LENGTH_SHORT).show();
+            } else {
+                NavHostFragment.findNavController(TripCreationFragment.this)
+                    .navigate(R.id.action_TripCreationFragment_to_DateFragment);
             }
+        });
+
+        binding.calendarButton.setOnClickListener(v -> {
+            materialDatePicker.show(getParentFragmentManager(), "Tag_picker");
+            materialDatePicker.addOnPositiveButtonClickListener(selection -> binding.dateRangeSelection.setText(materialDatePicker.getHeaderText()));
         });
     }
 
