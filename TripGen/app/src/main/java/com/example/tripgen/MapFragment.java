55be2ba967c9e8f306eff72473db757871d43a97
Ericson <e9so@uwaterloo.ca>
2023-07-23 20:06:33 -0400

    Added more API to get time and distance between 2 locations, auto complete, and clicking button to have multiple locations connected

--- TripGen/app/src/main/java/com/example/tripgen/MapFragment.java
+++ TripGen/app/src/main/java/com/example/tripgen/MapFragment.java
@@ -1,9 +1,12 @@
 package com.example.tripgen;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,6 +24,7 @@ import androidx.fragment.app.Fragment;
 import com.example.tripgen.databinding.FragmentMapBinding;
 import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -34,8 +38,11 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.OpeningHours;
 import com.google.android.libraries.places.api.model.PhotoMetadata;
 import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.api.net.FetchPhotoRequest;
 import com.google.android.libraries.places.api.net.FetchPhotoResponse;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
@@ -58,6 +65,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 123;
     private FragmentMapBinding binding;
@@ -65,17 +83,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap googleMap;
     private PlacesClient placesClient;
     private Polyline polyline;
-    private List<Place.Field> placeFields = Arrays.asList(
-            Place.Field.NAME,
-            Place.Field.ADDRESS,
-            //Place.Field.PHONE_NUMBER,
-            //Place.Field.WEBSITE_URI,
-            //Place.Field.RATING,
-            //Place.Field.USER_RATINGS_TOTAL,
-            //Place.Field.PRICE_LEVEL,
-            //Place.Field.OPENING_HOURS,
-            Place.Field.PHOTO_METADATAS
-    );
+
+    private static final String DIRECTIONS_API_BASE_URL = "https://maps.googleapis.com/maps/api/directions/json";
+    private static final String API_KEY = "AIzaSyC71z73qlGojykNfUrUXAmscdv8JGfzn8I";
+
 
     @Override
     public View onCreateView(
@@ -89,6 +100,30 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         Places.initialize(getActivity(), "AIzaSyC71z73qlGojykNfUrUXAmscdv8JGfzn8I");
         placesClient = Places.createClient(getActivity());
 
+        //Initialize AutocompleteSupportFragment
+        AutocompleteSupportFragment autocompleteSupportFragment =
+                (AutocompleteSupportFragment)getChildFragmentManager()
+                        .findFragmentById(R.id.autoCompleteTextViewSearch);
+
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,
+                Place.Field.NAME, Place.Field.LAT_LNG));
+
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                Toast.makeText(getActivity(), "Selected: " + place.getName(), Toast.LENGTH_SHORT).show();
+
+                googleMap.clear();
+                googleMap.addMarker(new MarkerOptions().position(place.getLatLng()));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 12));
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Toast.makeText(getActivity(), "Error: " + status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         //initialize MapView
         mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
@@ -96,24 +131,25 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         //get GoogleMap object
         mapView.getMapAsync(this);
 
-        EditText searchEditText = view.findViewById(R.id.searchEditText);
-        Button searchButton = view.findViewById(R.id.searchButton);
+        //save button init
+        Button saveButton = view.findViewById(R.id.searchButton2);
 
         //TESTING
+        getDistanceTime("Niagra Falls", "CN Tower", "driving");
         getCurrentLocation();
         getDetailsOfLocationToManipulate("Canada's Wonderland");
         getPictureOfLocationToManipulate("Canada's Wonderland");
 
         makeNearbyPlaceRequestToManipulate();
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String location = searchEditText.getText().toString();
 
-                LatLng latLng = getLatLngFromLocation(location);
-                googleMap.clear();
-                googleMap.addMarker(new MarkerOptions().position(latLng));
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ArrayList<String> locations = new ArrayList<>();
+                locations.add("Markham");
+                locations.add("CN Tower");
+                locations.add("Niagara Falls");
+                openGoogleMapsWithDirections(locations);
             }
         });
 
@@ -171,6 +207,68 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 Log.e("Error", "Exception: " + exception.getMessage());
             }
         });
+    }
+
+    //Example origin = University of Waterloo
+    //destination = CN Tower
+    //mode = driving, walking, transit
+    public void getDistanceTime(String origin, String destination, String mode){
+        getDirectionsDistanceTime(origin, destination, mode,
+                response -> {
+                    Log.d("Directions", "Distance and Duration: " + response);
+                },
+                error -> {
+                    Log.e("Directions", "Error: " + error.getMessage());
+                }
+        );
+    }
+    private void getDirectionsDistanceTime(String origin, String destination, String mode,
+                                                 Response.Listener<String> listener,
+                                                 Response.ErrorListener errorListener){
+        String url = buildUrl(origin, destination, mode);
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        // Parse the JSON response and extract distance and duration information
+                        Log.d("Response", " " + response);
+                        JSONArray routes = response.getJSONArray("routes");
+                        if (routes.length() > 0) {
+                            JSONObject route = routes.getJSONObject(0);
+                            JSONArray legs = route.getJSONArray("legs");
+                            if(legs.length() > 0){
+                                JSONObject leg = legs.getJSONObject(0);
+                                JSONObject distanceObj = leg.getJSONObject("distance");
+                                JSONObject durationObj = leg.getJSONObject("duration");
+
+                                String distanceText = distanceObj.getString("text");
+                                String durationText = durationObj.getString("text");
+
+                                listener.onResponse(distanceText + " (" + durationText + ")");
+                            }else{
+                                Log.d("legs.length", "less than 0" + legs);
+                            }
+
+                        }else{
+                            Log.d("Routes.length", "less than 0" + routes);
+                        }
+                    } catch (JSONException e) {
+                        errorListener.onErrorResponse(new VolleyError("Error parsing response"));
+                    }
+                },
+                error -> errorListener.onErrorResponse(new VolleyError("Error getting directions"))
+        );
+        queue.add(jsonObjectRequest);
+
+    }
+
+    private static String buildUrl(String origin, String destination, String mode){
+        String encodeOrigin = Uri.encode(origin);
+        String encodeDest = Uri.encode(destination);
+
+        return DIRECTIONS_API_BASE_URL + "?origin=" + encodeOrigin + "&destination="
+                + encodeDest + "&mode=" + mode + "&key=" + API_KEY;
     }
 
     private void testBitMapInAlertDialog(Bitmap bitmap) {
@@ -343,6 +441,25 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void makeNearbyPlaceRequest(FetchPlaceArrayListCallback callback) {
+        List<Place.Type> desiredTypes = Arrays.asList(Place.Type.RESTAURANT,
+                                                Place.Type.TOURIST_ATTRACTION,
+                                                Place.Type.PARK,
+                                                Place.Type.MUSEUM,
+                                                Place.Type.SHOPPING_MALL,
+                                                Place.Type.UNIVERSITY
+        );
+        List<Place.Field> placeFields = Arrays.asList(
+                Place.Field.NAME,
+                Place.Field.ADDRESS,
+                //Place.Field.PHONE_NUMBER,
+                //Place.Field.WEBSITE_URI,
+                Place.Field.RATING,
+                Place.Field.USER_RATINGS_TOTAL,
+                Place.Field.PRICE_LEVEL,
+                //Place.Field.OPENING_HOURS,
+                Place.Field.PHOTO_METADATAS,
+                Place.Field.TYPES
+        );
         FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(placeFields)
                 .build();
 
@@ -356,7 +473,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 FindCurrentPlaceResponse response = task.getResult();
                 for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
                     Place place = placeLikelihood.getPlace();
-                    placeArrayList.add(place);
+                    if(place.getTypes().stream().anyMatch(desiredTypes::contains)){
+                        placeArrayList.add(place);
+                    }
                 }
                 callback.onPlaceArrayListFetched(placeArrayList);
             }else{
@@ -366,8 +485,66 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         });
     }
 
+    private void openGoogleMapsWithDirections(ArrayList<String> locations) {
+        ArrayList<LatLng> locationsLatLng = new ArrayList<>();
+        if (locations.size() < 2) {
+            return;
+        }
+
+        //convert locations to LatLng
+        for(String location : locations){
+            locationsLatLng.add(getLatLngFromLocation(location));
+        }
+
+        StringBuilder uriBuilder = new StringBuilder("https://www.google.com/maps/dir/?api=1");
+
+        uriBuilder.append("&origin=")
+                .append(locationsLatLng.get(0).latitude)
+                .append(",")
+                .append(locationsLatLng.get(0).longitude);
+        uriBuilder.append("&destination=")
+                .append(locationsLatLng.get(locations.size()-1).latitude)
+                .append(",")
+                .append(locationsLatLng.get(locations.size()-1).longitude);
+
+        if(locationsLatLng.size() > 2){
+            uriBuilder.append("&waypoints=");
+            for(int i=1; i<locationsLatLng.size()-1; ++i){
+                uriBuilder.append(locationsLatLng.get(i).latitude)
+                        .append(",")
+                        .append(locationsLatLng.get(i).longitude);
+                if(i<locationsLatLng.size()-2){
+                    uriBuilder.append("!");
+                }
+            }
+        }
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uriBuilder.toString()));
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        List<ResolveInfo> activities = getActivity().getPackageManager().queryIntentActivities(mapIntent, 0);
+
+        if(activities != null && !activities.isEmpty()){
+            startActivity(mapIntent);
+        }else{
+            Toast.makeText(getContext(), "Please install the Google Maps app to open Google Maps", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void getDetailsOfLocation(String location, FetchPlaceCallback callback) {
         AtomicReference<String> placeId = new AtomicReference<>();
+        List<Place.Field> placeFields = Arrays.asList(
+                Place.Field.NAME,
+                Place.Field.ADDRESS,
+                Place.Field.PHONE_NUMBER,
+                Place.Field.WEBSITE_URI,
+                Place.Field.RATING,
+                Place.Field.USER_RATINGS_TOTAL,
+                Place.Field.PRICE_LEVEL,
+                Place.Field.OPENING_HOURS,
+                Place.Field.PHOTO_METADATAS,
+                Place.Field.TYPES
+        );
 
         AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
         FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
@@ -434,9 +611,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return latLng;
     }
 
+    //gets the phone actual location
     private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted, request it
             ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
         FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
@@ -446,7 +623,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                         double latitude = location.getLatitude();
                         double longitude = location.getLongitude();
                         Log.i("CurrentLocation", "Latitude: " + latitude + ", Longitude: " + longitude);
-                        // Use the latitude and longitude for further processing
                     } else {
                         Log.e("CurrentLocation", "Location is null");
                     }
